00774ca132142b8a1fcb2cc3c5c0ec56c71879c6
Shreya rai <shreyaraai28@gmail.com>
2026-04-29 15:52:57 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -25,6 +25,7 @@ model.eval()
 # CREATE GAME
 # -----------------------------
 game = SnakeGameAI()
+
 # -----------------------------
 # GET STATE FUNCTION
 # -----------------------------
@@ -32,15 +33,18 @@ def get_state(game):
 
     head = game.snake[0]
 
-    point_l = pygame.Vector2(head.x - 20, head.y)
-    point_r = pygame.Vector2(head.x + 20, head.y)
-    point_u = pygame.Vector2(head.x, head.y - 20)
-    point_d = pygame.Vector2(head.x, head.y + 20)
+    x = head[0]
+    y = head[1]
 
-    dir_l = game.direction == pygame.K_LEFT
-    dir_r = game.direction == pygame.K_RIGHT
-    dir_u = game.direction == pygame.K_UP
-    dir_d = game.direction == pygame.K_DOWN
+    point_l = (x - 20, y)
+    point_r = (x + 20, y)
+    point_u = (x, y - 20)
+    point_d = (x, y + 20)
+
+    dir_l = game.direction == "LEFT"
+    dir_r = game.direction == "RIGHT"
+    dir_u = game.direction == "UP"
+    dir_d = game.direction == "DOWN"
 
     state = [
 
@@ -69,10 +73,10 @@ def get_state(game):
         dir_d,
 
         # Food location
-        game.food.x < game.head.x,
-        game.food.x > game.head.x,
-        game.food.y < game.head.y,
-        game.food.y > game.head.y
+        game.food[0] < x,
+        game.food[0] > x,
+        game.food[1] < y,
+        game.food[1] > y
     ]
 
     return np.array(state, dtype=int)
@@ -84,22 +88,22 @@ running = True
 
 while running:
 
-    # CLOSE WINDOW EVENT
+    # CLOSE WINDOW
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # GET CURRENT STATE
+    # CURRENT STATE
     state = get_state(game)
 
     state0 = torch.tensor(state, dtype=torch.float)
 
-    # PREDICT MOVE
+    # MODEL PREDICTION
     prediction = model(state0)
 
     move = torch.argmax(prediction).item()
 
-    # CONVERT TO MOVE FORMAT
+    # MOVE FORMAT
     final_move = [0, 0, 0]
     final_move[move] = 1
 
@@ -115,6 +119,6 @@ while running:
         running = False
 
 # -----------------------------
-# QUIT GAME
+# QUIT
 # -----------------------------
 pygame.quit()
